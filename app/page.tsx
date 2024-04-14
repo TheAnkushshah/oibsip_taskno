18f5482d342dcdce10f6b03796291f09afa4d7cb
Ankush Shah <ankushsahvlogs@gmail.com>
2024-04-14 21:09:49 +0530

    New Background

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const font = Poppins({
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <main className="flex h-full flex-col items-center justify-center" style={{ backgroundImage: 'url("https://wallpapers.com/images/hd/the-simpsons-santa-s-little-helper-tng9g2yplu92mqk3.jpg")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className="space-y-6 text-center">
         <h1 className={cn(
           "text-6xl font-semibold text-white drop-shadow-md",
