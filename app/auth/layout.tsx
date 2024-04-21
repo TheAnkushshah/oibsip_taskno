371e3623e3cebd32ef292c825bc76a683c65cb7b
Ankush Shah <ankushsahvlogs@gmail.com>
2024-04-21 12:54:35 +0530

    fix

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -4,7 +4,7 @@ const AuthLayout = ({
   children: React.ReactNode
 }) => {
   return ( 
-    <div className="h-full flex items-center justify-center" style={{ backgroundImage: 'url("public\Pro-bg.jpg")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <div className="h-full flex items-center justify-center" style={{ backgroundImage: 'url("public/Pro-bg.jpg")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
       {children}
     </div>
    );
