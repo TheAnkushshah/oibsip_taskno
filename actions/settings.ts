615182cc61dad034e7f8810f7f9dcc887d30950d
Ankush Shah <ankushsahvlogs@gmail.com>
2025-01-25 03:31:27 +0530

    hey

--- actions/settings.ts
+++ actions/settings.ts
@@ -11,19 +11,17 @@ import { currentUser } from "@/lib/auth";
 import { generateVerificationToken } from "@/lib/tokens";
 import { sendVerificationEmail } from "@/lib/mail";
 
-export const settings = async (
-  values: z.infer<typeof SettingsSchema>
-) => {
+export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   const user = await currentUser();
 
-  if (!user) {
-    return { error: "Unauthorized" }
+  if (!user || !user.id) {
+    return { error: "Unauthorized" };
   }
 
   const dbUser = await getUserById(user.id);
 
   if (!dbUser) {
-    return { error: "Unauthorized" }
+    return { error: "Unauthorized" };
   }
 
   if (user.isOAuth) {
@@ -37,34 +35,23 @@ export const settings = async (
     const existingUser = await getUserByEmail(values.email);
 
     if (existingUser && existingUser.id !== user.id) {
-      return { error: "Email already in use!" }
+      return { error: "Email already in use!" };
     }
 
-    const verificationToken = await generateVerificationToken(
-      values.email
-    );
-    await sendVerificationEmail(
-      verificationToken.email,
-      verificationToken.token,
-    );
+    const verificationToken = await generateVerificationToken(values.email);
+    await sendVerificationEmail(verificationToken.email, verificationToken.token);
 
     return { success: "Verification email sent!" };
   }
 
   if (values.password && values.newPassword && dbUser.password) {
-    const passwordsMatch = await bcrypt.compare(
-      values.password,
-      dbUser.password,
-    );
+    const passwordsMatch = await bcrypt.compare(values.password, dbUser.password);
 
     if (!passwordsMatch) {
       return { error: "Incorrect password!" };
     }
 
-    const hashedPassword = await bcrypt.hash(
-      values.newPassword,
-      10,
-    );
+    const hashedPassword = await bcrypt.hash(values.newPassword, 10);
     values.password = hashedPassword;
     values.newPassword = undefined;
   }
@@ -73,7 +60,7 @@ export const settings = async (
     where: { id: dbUser.id },
     data: {
       ...values,
-    }
+    },
   });
 
   update({
@@ -82,8 +69,8 @@ export const settings = async (
       email: updatedUser.email,
       isTwoFactorEnabled: updatedUser.isTwoFactorEnabled,
       role: updatedUser.role,
-    }
+    },
   });
 
-  return { success: "Settings Updated!" }
-}
+  return { success: "Settings Updated!" };
+};
